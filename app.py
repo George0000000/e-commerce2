ba31fefc12bf7d4535e8f2c8c33ea6c28193acb5
GreyRose <greyrose>
2023-10-26 10:51:01 +0700

    i like this functions

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask_admin.contrib.sqla.fields import QuerySelectField
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_migrate import Migrate
 from wtforms import SelectField
+from flask_admin.form import Select2Widget
+from wtforms import Form, StringField, TextAreaField, FloatField
 
 
 app = Flask(__name__)
@@ -68,20 +71,20 @@ menu = [
 ]
 
 
+class SubcategoryForm(Form):
+    name = StringField('Name')
+    category = QuerySelectField('Category', query_factory=Subcategory.query_factory, widget=Select2Widget())  # Используем Select2Widget для выпадающего списка
+
+
 admin.add_view(ModelView(Category, db.session))
 
 
 class SubcategoryModelView(ModelView):
-    column_list = ['name', 'category']
-    form_columns = ['name', 'category']
-    # Укажите query_factory для поля category
-    form_extra_fields = {
-        'category': SelectField('Category', choices=[], coerce=int)
-    }
-    def on_form_prefill(self, form, id):
-        form.category.choices = [(category.id, category.name) for category in Category.query.all()]
-    def on_model_change(self, form, model, is_created):
-        model.category = Category.query.get(form.category.data)
+    column_list = ['name', 'category']  # Список отображаемых колонок
+    form_columns = ['name', 'category']  # Колонки, которые отображаются в форме редактирования
+    form = SubcategoryForm  # Используем нашу форму
+    column_searchable_list = ['name']  # Список колонок, в которых можно выполнять поиск
+    column_filters = ['category']  # Список колонок, по которым можно фильтровать данные
 
 
 admin.add_view(SubcategoryModelView(Subcategory, db.session))
